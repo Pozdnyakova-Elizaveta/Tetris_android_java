a601882f9abf032e6e9510d2eac647f4cf6bcba8
Ulyana <uliana.malakhova@yandex.ru>
2023-01-24 00:22:52 +0700

    Комментарии к меню и правилам

--- app/src/main/java/com/example/tetris/Menu.java
+++ app/src/main/java/com/example/tetris/Menu.java
@@ -22,9 +22,9 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
         RelativeLayout layout = new RelativeLayout(this);
-        layout.setBackgroundResource(R.drawable.background);
+        layout.setBackgroundResource(R.drawable.background);//установка фона
         layout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-        title = new ImageView(this);
+        title = new ImageView(this);//установка картинки tetris
         title.setImageResource(R.drawable.title);
         RelativeLayout.LayoutParams params_title = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
                 RelativeLayout.LayoutParams.MATCH_PARENT);
@@ -32,54 +32,54 @@ public class Menu extends AppCompatActivity {
         title.setLayoutParams(params_title);
         layout.addView(title);
 
-        AlphaAnimation animation1 = new AlphaAnimation(0.0f, 1.0f);
+        AlphaAnimation animation1 = new AlphaAnimation(0.0f, 1.0f);//добавление анимации к изображению
         animation1.setDuration(1500);
         animation1.setStartOffset(2000);
         animation1.setFillAfter(true);
         title.startAnimation(animation1);
-        button_game = new ImageButton(this);
+        button_game = new ImageButton(this);//кнопка перехода к игре
         button_game.setBackgroundColor(Color.TRANSPARENT);
-        button_game.setImageDrawable(getDrawable(R.drawable.start));
+        button_game.setImageDrawable(getDrawable(R.drawable.start));//установка изображения
         LinearLayout.LayoutParams param_button_game = new LinearLayout.LayoutParams(300,150);
-        param_button_game.leftMargin = 385;
+        param_button_game.leftMargin = 385;//расположение кнопки на экране
         param_button_game.topMargin = 1500;
         button_game.setLayoutParams(param_button_game);
         layout.addView(button_game);
         button_game.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Обработка нажатия
+                // Обработка нажатия - переход к игре
                 Intent intent = new Intent(Menu.this, Game.class);
                 startActivity(intent);
             }
         });
         setContentView(layout);
-        button_rules = new ImageButton(this);
+        button_rules = new ImageButton(this);//кнопка перехода к правилам игры
         button_rules.setBackgroundColor(Color.TRANSPARENT);
-        button_rules.setImageDrawable(getDrawable(R.drawable.rules));
+        button_rules.setImageDrawable(getDrawable(R.drawable.rules));//установка изображения
         LinearLayout.LayoutParams param_button_rules = new LinearLayout.LayoutParams(300,150);
-        param_button_rules.leftMargin = 750;
+        param_button_rules.leftMargin = 750;//расположение кнопки
         param_button_rules.topMargin = 80;
         button_rules.setLayoutParams(param_button_rules);
         layout.addView(button_rules);
         button_rules.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Обработка нажатия
+                // Обработка нажатия - переход к правилам
                 Intent intent = new Intent(Menu.this, Rules.class);
                 startActivity(intent);
             }
         });
         setContentView(layout);
-        button_exit = new ImageButton(this);
+        button_exit = new ImageButton(this);//кнопка выхода
         button_exit.setBackgroundColor(Color.TRANSPARENT);
-        button_exit.setImageDrawable(getDrawable(R.drawable.menu));
+        button_exit.setImageDrawable(getDrawable(R.drawable.menu));//установка изображения
         LinearLayout.LayoutParams param_button_exit = new LinearLayout.LayoutParams(300,150);
-        param_button_exit.leftMargin = 750;
+        param_button_exit.leftMargin = 750;//расположение кнопки на экране
         param_button_exit.topMargin = 1700;
         button_exit.setLayoutParams(param_button_exit);
         layout.addView(button_exit);
         button_exit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Обработка нажатия
+                // Обработка нажатия - выход
                 System.exit(0);
             }
         });
--- app/src/main/java/com/example/tetris/Rules.java
+++ app/src/main/java/com/example/tetris/Rules.java
@@ -20,39 +20,32 @@ public class Rules extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
         RelativeLayout layout = new RelativeLayout(this);
-        layout.setBackgroundResource(R.drawable.background);
+        layout.setBackgroundResource(R.drawable.background);//установка фона
         layout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
-        button_back = new ImageButton(this);
+        button_back = new ImageButton(this);//кнопка выхода в меню
         button_back.setBackgroundColor(Color.TRANSPARENT);
         button_back.setImageDrawable(getDrawable(R.drawable.menu));
         LinearLayout.LayoutParams param_button_exit = new LinearLayout.LayoutParams(300,150);
-        param_button_exit.leftMargin = 750;
+        param_button_exit.leftMargin = 750;//расположение кнопки
         param_button_exit.topMargin = 1700;
         button_back.setLayoutParams(param_button_exit);
         layout.addView(button_back);
-        textView = new TextView(this);
-        // установка фонового цвета
+        textView = new TextView(this);// установка текста правил
         textView.setBackgroundColor(Color.TRANSPARENT);
-        // установка цвета текста
         textView.setTextColor(getColor(R.color.white));
-        // делаем все буквы заглавными
-        textView.setAllCaps(true);
+        textView.setAllCaps(true); // делаем буквы заглавными
         LinearLayout.LayoutParams param_button_exit1 = new LinearLayout.LayoutParams(300,150);
-        // устанавливаем вравнивание текста по центру
-        textView.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);
-        // устанавливаем текста
+        textView.setTextAlignment(TextView.TEXT_ALIGNMENT_CENTER);// вравнивание текста по центру
         textView.setText("Сверху вниз падают фигуры разных цветов и форм. Фигуру можно двигать по горизонтали поворотом экрана в сторону необходимого направления движения фигуры.  С помощью кнопок ее можно вращать по и против часовой стрелки, также соответствующей кнопкой фигуру можно опустить в крайнее нижнее положение. При положении по краям, если движение дальше недоступно, фигура остаётся в последнем доступном положении. Задача игрока собирать фигуры в полностью заполненный ряд. В таком случае ряд исчезает и игрок получает баллы. После того как игрок соберет один ряд, он  получает 10 баллов, за два ряда - 30, за три - 60. Время падения фигур в течении игры уменьшается, начинается игра с падения фигур в течении 2 секунд и сокращается до 0.5 секунды. \n" +
                 "\n" +
                 "\n" +
                 "Разработчики игры - Малахова Ульяна, Позднякова Елизавета");
-        // установка шрифта
-        textView.setTypeface(Typeface.create("casual", Typeface.NORMAL));
-        // устанавливаем высоту текста
-        textView.setTextSize(17);
+        textView.setTypeface(Typeface.create("casual", Typeface.NORMAL));// установка шрифта
+        textView.setTextSize(17);// устанавливаем высоту текста
         layout.addView(textView);
         button_back.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Обработка нажатия
+                // Обработка нажатия - выход в меню
                 Intent intent = new Intent(Rules.this, Menu.class);
                 startActivity(intent);
             }
