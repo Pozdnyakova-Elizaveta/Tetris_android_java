1621c1b16366f9f2f47c230b7c3a9ed365b46d96
Ulyana <uliana.malakhova@yandex.ru>
2023-01-23 23:51:52 +0700

    Визуальное расположение кнопок

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, Game.class);
+        Intent intent = new Intent(this, Menu.class);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/tetris/Menu.java
+++ app/src/main/java/com/example/tetris/Menu.java
@@ -2,13 +2,49 @@ package com.example.tetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class Menu extends AppCompatActivity {
-
+    private ImageButton button_game;
+    private ImageButton button_rules;
+    private ImageButton button_exit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        RelativeLayout layout = new RelativeLayout(this);
+        layout.setBackgroundResource(R.drawable.background);
+        layout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        button_game = new ImageButton(this);
+        button_game.setBackgroundColor(Color.TRANSPARENT);
+        button_game.setImageDrawable(getDrawable(R.drawable.start));
+        LinearLayout.LayoutParams param_button_game = new LinearLayout.LayoutParams(300,150);
+        param_button_game.leftMargin = 385;
+        param_button_game.topMargin = 1500;
+        button_game.setLayoutParams(param_button_game);
+        layout.addView(button_game);
+        setContentView(layout);
+        button_rules = new ImageButton(this);
+        button_rules.setBackgroundColor(Color.TRANSPARENT);
+        button_rules.setImageDrawable(getDrawable(R.drawable.rules));
+        LinearLayout.LayoutParams param_button_rules = new LinearLayout.LayoutParams(300,150);
+        param_button_rules.leftMargin = 750;
+        param_button_rules.topMargin = 80;
+        button_rules.setLayoutParams(param_button_rules);
+        layout.addView(button_rules);
+        setContentView(layout);
+        button_exit = new ImageButton(this);
+        button_exit.setBackgroundColor(Color.TRANSPARENT);
+        button_exit.setImageDrawable(getDrawable(R.drawable.menu));
+        LinearLayout.LayoutParams param_button_exit = new LinearLayout.LayoutParams(300,150);
+        param_button_exit.leftMargin = 750;
+        param_button_exit.topMargin = 1700;
+        button_exit.setLayoutParams(param_button_exit);
+        layout.addView(button_exit);
+        setContentView(layout);
     }
 }
